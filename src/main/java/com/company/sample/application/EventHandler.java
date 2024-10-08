03e57031f768a1fb20501f144d376a05c9c6fe6a
Noln-git <noln.nekosuki@gmail.com>
2024-10-09 00:06:09 +0900

    Adding EventHandler.java file

---
+++ src/main/java/com/company/sample/application/EventHandler.java
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>PermanentRedirect</Code><Message>The bucket you are attempting to access must be addressed using the specified endpoint. Please send all future requests to this endpoint.</Message><Endpoint>s3.amazonaws.com</Endpoint><Bucket>awsdemoworkshops</Bucket><RequestId>9E1A948D8DRY21TK</RequestId><HostId>aFD13D/mpgLH1+TKehkL57e417vXhadWK3mW0wYfmui6WspgfbRAPexzDg4AewMRaGMmek6rExI=</HostId></Error>
